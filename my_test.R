e4686927ac30ba7db6f8a35130a2f6492baebbc7
scott-stoltzman-consulting <scott@stoltzmanconsulting.com>
2021-01-27 15:16:25 -0700

    For a TON of failures to see what's going on

--- my_test.R
+++ my_test.R
@@ -3,22 +3,50 @@ test_that("multiplication works", {
 })
 
 
-test_that("Environment variable 1 exists", {
+test_that("Environment variable 1 exists left", {
   expect_equal(Sys.getenv('MY_VARIABLE'), 'i_want_this')
 })
 
-test_that("Environment variable 1 exists", {
+test_that("Environment variable 1 exists right", {
   expect_equal('i_want_this', Sys.getenv('MY_VARIABLE'))
 })
 
-test_that("Environment variable 2 exists", {
+test_that("Environment variable 1 exists blank left", {
+  expect_equal(Sys.getenv('MY_VARIABLE'), '')
+})
+
+test_that("Environment variable 1 exists blank right", {
+  expect_equal('', Sys.getenv('MY_VARIABLE'))
+})
+
+
+
+
+test_that("Environment variable 2 left", {
+  expect_equal(Sys.getenv('MY_VARIABLE2'), 'i_want_this_2')
+})
+
+test_that("Environment variable 2 right", {
+  expect_equal('i_want_this_2', Sys.getenv('MY_VARIABLE2'))
+})
+
+
+test_that("Environment variable 2 exists star left", {
   expect_equal(Sys.getenv('MY_VARIABLE2'), "***")
 })
 
-test_that("Environment variable 2 exists", {
+test_that("Environment variable 2 exists star right", {
   expect_equal("***", Sys.getenv('MY_VARIABLE2'))
 })
 
 test_that("Environment variable 2 exists", {
+  expect_equal(Sys.getenv('MY_VARIABLE2'), "")
+})
+
+test_that("Environment variable 2 exists", {
+  expect_equal("", Sys.getenv('MY_VARIABLE2'))
+})
+
+test_that("Environment variable blah exists", {
   expect_equal('blah_1', 'blah_2')
 })
