5ec18e0ab561585fa983bd54f087bbe78f9cafba
scott-stoltzman-consulting <scott@stoltzmanconsulting.com>
2021-01-27 14:53:41 -0700

    Change order?

--- my_test.R
+++ my_test.R
@@ -2,10 +2,12 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("Environment variable exists", {
-  expect_equal(Sys.getenv('MY_VARIABLE'), 'i_want_this')
-})
 
 test_that("Environment variable 2 exists", {
   expect_equal(Sys.getenv('MY_VARIABLE2'), 'i_want_this_2')
+})
+
+
+test_that("Environment variable exists", {
+  expect_equal(Sys.getenv('MY_VARIABLE'), 'i_want_this')
 })
