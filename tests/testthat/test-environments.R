cfd72d79942a3875baaa2cc182770456c91d8bfc
scott-stoltzman-consulting <scott@stoltzmanconsulting.com>
2021-02-04 15:25:19 -0700

    Expect all to pass!

--- tests/testthat/test-environments.R
+++ tests/testthat/test-environments.R
@@ -6,7 +6,3 @@ test_that("Environment variable 1 exists", {
 test_that("Environment variable 2 exists", {
   expect_equal(Sys.getenv('MY_SECRET2'), 'i_want_this_2')
 })
-
-test_that("Supposed to fail", {
-  expect_equal(1, 2)
-})
