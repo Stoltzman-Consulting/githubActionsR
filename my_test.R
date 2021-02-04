471b73f64d0e37701118c6dbb8804b6e4254f8b8
scott-stoltzman-consulting <scott@stoltzmanconsulting.com>
2021-02-04 15:12:09 -0700

    fix naming

--- my_test.R
+++ my_test.R
@@ -3,50 +3,50 @@ test_that("multiplication works", {
 })
 
 
-test_that("Environment variable 1 exists left", {
+test_that("Environment variable 1 exists left (should pass)", {
   expect_equal(Sys.getenv('MY_SECRET'), 'i_want_this')
 })
 
-test_that("Environment variable 1 exists right", {
+test_that("Environment variable 1 exists right (should pass)", {
   expect_equal('i_want_this', Sys.getenv('MY_SECRET'))
 })
 
-test_that("Environment variable 1 exists blank left", {
+test_that("Environment variable 1 exists blank left (should fail)", {
   expect_equal(Sys.getenv('MY_SECRET'), '')
 })
 
-test_that("Environment variable 1 exists blank right", {
+test_that("Environment variable 1 exists blank right (should fail)", {
   expect_equal('', Sys.getenv('MY_SECRET'))
 })
 
 
 
 
-test_that("Environment variable 2 left", {
+test_that("Environment variable 2 left (should pass)", {
   expect_equal(Sys.getenv('MY_SECRET2'), 'i_want_this_2')
 })
 
-test_that("Environment variable 2 right", {
+test_that("Environment variable 2 right (should pass)", {
   expect_equal('i_want_this_2', Sys.getenv('MY_SECRET2'))
 })
 
 
-test_that("Environment variable 2 exists star left", {
+test_that("Environment variable 2 exists star left (should fail)", {
   expect_equal(Sys.getenv('MY_SECRET2'), "***")
 })
 
-test_that("Environment variable 2 exists star right", {
+test_that("Environment variable 2 exists star right (should fail)", {
   expect_equal("***", Sys.getenv('MY_SECRET2'))
 })
 
-test_that("Environment variable 2 exists", {
+test_that("Environment variable 2 exists blank left (should fail)", {
   expect_equal(Sys.getenv('MY_SECRET2'), "")
 })
 
-test_that("Environment variable 2 exists", {
+test_that("Environment variable 2 exists blank right (should fail)", {
   expect_equal("", Sys.getenv('MY_SECRET2'))
 })
 
-test_that("Environment variable blah exists", {
+test_that("Environment variable blah exists (should fail)", {
   expect_equal('blah_1', 'blah_2')
 })
